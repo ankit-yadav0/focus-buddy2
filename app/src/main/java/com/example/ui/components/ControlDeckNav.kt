623411020adf48fdad2da79f55e767a585e79178
ankit-yadav0 <ankitvnsyadav05@gmail.com>
2026-07-29 08:24:54 +0530

    Update ControlDeckNav.kt

--- app/src/main/java/com/example/ui/components/ControlDeckNav.kt
+++ app/src/main/java/com/example/ui/components/ControlDeckNav.kt
@@ -3,7 +3,7 @@ package com.example.ui.components
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
